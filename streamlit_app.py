ce7fe4ba8e6965b880ebd88a4b878b3ec864ed21
59762 <alsha9762@nmbu.no>
2026-09-18 20:05:32 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,6 @@
+import streamlit as st
 
+st.set_page_config(page_title="IND320 Dashboard", layout="wide")
+
+st.title("IND320 – Project Dashboard")
+st.write("Velkommen! Bruk menyen til venstre for å navigere mellom sidene.")
